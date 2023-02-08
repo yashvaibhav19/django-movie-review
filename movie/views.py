81fdf9f6f3117782c790c4dfb6523ea181e692a7
Yash <yashvaibhav19@gmail.com>
2023-02-08 07:20:54 +0530

    submit form in same page

--- movie/views.py
+++ movie/views.py
@@ -2,7 +2,11 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def home(request):
-    return HttpResponse('<h1>Welcome to Home page')
+    searchTerm = request.GET.get('searchMovie')
+    return render( request , 'home.html',{
+        'name':'Yash Vaibhav',
+        'searchTerm':searchTerm
+    })
 
 def about(request):
     return HttpResponse('<h1>Welcome to About page</h1>')
